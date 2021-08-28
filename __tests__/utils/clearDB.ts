0e51b1623d74b632a938b6d4c7bb4472f2a7f366
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-08-28 08:43:11 -0300

    Added to_do table to be cleared

--- __tests__/utils/clearDB.ts
+++ __tests__/utils/clearDB.ts
@@ -1,9 +1,10 @@
 import prisma from '../../src/prisma/prisma'
 
 async function clearDB(notDisconnect?: boolean) {
+  const deleteToDos = prisma.toDo.deleteMany()
   const deleteUsers = prisma.user.deleteMany()
 
-  await prisma.$transaction([deleteUsers])
+  await prisma.$transaction([deleteToDos, deleteUsers])
 
   if (!notDisconnect) await prisma.$disconnect()
 }

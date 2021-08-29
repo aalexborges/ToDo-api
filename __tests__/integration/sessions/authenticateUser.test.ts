a780617ad2989c9c6b832e27bfc6a6e8954b2533
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-08-29 13:34:20 -0300

    Added one more test to authenticateUser

--- __tests__/integration/sessions/authenticateUser.test.ts
+++ __tests__/integration/sessions/authenticateUser.test.ts
@@ -17,6 +17,23 @@ describe('Authenticate user', () => {
     expect(response.body).toMatchObject({ error: 'Email or password invalid' })
   })
 
+  it('should not authenticate if credentials are invalid', async () => {
+    const data = { email: internet.email(), password: internet.password(10) }
+
+    const user = await request(app)
+      .post('/users')
+      .send({ ...data, name: name.findName() })
+
+    expect(user.status).toBe(201)
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({ ...data, password: internet.password(9) })
+
+    expect(response.status).toBe(401)
+    expect(response.body).toMatchObject({ error: 'Email or password invalid' })
+  })
+
   it('must authenticate when credentials are valid', async () => {
     const data = { email: internet.email(), password: internet.password(10) }
 

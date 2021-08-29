396b64d2a1b6f9ffcf3b4d8707fb3384f167545a
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-08-29 13:29:00 -0300

    The refreshToken table has been added to the list for cleanup

--- __tests__/utils/clearDB.ts
+++ __tests__/utils/clearDB.ts
@@ -2,9 +2,10 @@ import prisma from '../../src/prisma/prisma'
 
 async function clearDB(notDisconnect?: boolean) {
   const deleteToDos = prisma.toDo.deleteMany()
+  const deleteRefreshToken = prisma.refreshToken.deleteMany()
   const deleteUsers = prisma.user.deleteMany()
 
-  await prisma.$transaction([deleteToDos, deleteUsers])
+  await prisma.$transaction([deleteToDos, deleteRefreshToken, deleteUsers])
 
   if (!notDisconnect) await prisma.$disconnect()
 }

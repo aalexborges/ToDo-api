3d61fd6a5e6c7a24f4ceea87edfb8d2b69f3a87b
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-08-30 08:55:27 -0300

    Route /refreshToken created

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
 import AuthenticateUserController from '../controllers/sessions/AuthenticateUserController'
+import RefreshTokenController from '../controllers/sessions/RefreshTokenController'
 
 const authenticateUser = new AuthenticateUserController()
+const refreshToken = new RefreshTokenController()
 
 const sessionsRoutes = Router()
 
 sessionsRoutes.post('/', authenticateUser.handle)
+sessionsRoutes.post('/refreshToken', refreshToken.handle)
 
 export default sessionsRoutes

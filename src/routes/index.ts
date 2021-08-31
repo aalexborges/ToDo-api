d3fec479a7e9f3301257245ae40bb9fde1ae6963
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-08-31 10:01:31 -0300

    The /toDos route has been created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import usersRoutes from './users.routes'
 import sessionsRoutes from './sessions.routes'
+import toDosRoutes from './toDos.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/sessions', sessionsRoutes)
+routes.use('/toDos', toDosRoutes)
 
 export default routes

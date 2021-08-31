d8d2c5ccf32aaa059f4ab64a5ccbb854074361fa
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-08-31 15:50:24 -0300

    Created get route to /toDos

--- src/routes/toDos.routes.ts
+++ src/routes/toDos.routes.ts
@@ -2,14 +2,17 @@ import { Router } from 'express'
 
 // Controllers
 import CreateToDoController from '../controllers/toDo/CreateToDoController'
+import GetToDosController from '../controllers/toDo/GetToDosController'
 
 // Middlewares
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const createToDo = new CreateToDoController()
+const getToDos = new GetToDosController()
 
 const toDosRoutes = Router()
 
+toDosRoutes.get('/', ensureAuthenticated, getToDos.handle)
 toDosRoutes.post('/', ensureAuthenticated, createToDo.handle)
 
 export default toDosRoutes

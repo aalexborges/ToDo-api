a868bc63e6d4db0d061dfe780d0a0ac3ec8196ce
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-01 16:35:31 -0300

    Changed id parameter to refreshToken in refreshTokenValidation

--- src/validations/refreshToken.validation.ts
+++ src/validations/refreshToken.validation.ts
@@ -1,11 +1,11 @@
 import * as Yup from 'yup'
 
-const refreshTokenValidation = async (id?: string) => {
+const refreshTokenValidation = async (refreshToken?: string) => {
   const schema = Yup.object().shape({
-    id: Yup.string().required().uuid().trim(),
+    refreshToken: Yup.string().required().uuid().trim(),
   })
 
-  await schema.validate({ id }, { abortEarly: false })
+  await schema.validate({ refreshToken }, { abortEarly: false })
 }
 
 export default refreshTokenValidation

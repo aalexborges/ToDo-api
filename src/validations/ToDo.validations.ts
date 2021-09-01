8f74763ee5f18b2c90375062bf4591067062054e
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-01 10:29:10 -0300

    Created the delete function in ToDoValidations

--- src/validations/ToDo.validations.ts
+++ src/validations/ToDo.validations.ts
@@ -4,6 +4,11 @@ interface VCreateToDoDate {
   task?: string
 }
 
+interface VDeleteToDoDate {
+  id?: string
+  userId?: string
+}
+
 class ToDoValidations {
   static async create({ task }: VCreateToDoDate) {
     const schema = Yup.object().shape({
@@ -11,6 +16,15 @@ class ToDoValidations {
     })
 
     await schema.validate({ task }, { abortEarly: false })
+  }
+
+  static async delete({ id, userId }: VDeleteToDoDate) {
+    const schema = Yup.object().shape({
+      id: Yup.string().required().uuid().trim(),
+      userId: Yup.string().required().uuid().trim(),
+    })
+
+    await schema.validate({ id, userId }, { abortEarly: true })
   }
 }
 

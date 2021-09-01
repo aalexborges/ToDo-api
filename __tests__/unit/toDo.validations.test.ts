e4376ab9b3f4c11f6b1b3aee5d64c8cab6ebc91f
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-01 10:29:54 -0300

    Created ToDoValidations delete function test

--- __tests__/unit/toDo.validations.test.ts
+++ __tests__/unit/toDo.validations.test.ts
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid'
 import { ValidationError } from 'yup'
 
 import ToDoValidations from '../../src/validations/ToDo.validations'
@@ -13,5 +14,19 @@ describe('Test create validation', () => {
     const data = { task: 'Task' }
 
     await expect(ToDoValidations.create(data)).resolves.toBeUndefined()
+  })
+})
+
+describe('Test delete validation', () => {
+  it('should return an error when the data is invalid', async () => {
+    await expect(ToDoValidations.delete({})).rejects.toBeInstanceOf(
+      ValidationError
+    )
+  })
+
+  it('should return undefined when data is valid', async () => {
+    const data = { id: uuid(), userId: uuid() }
+
+    await expect(ToDoValidations.delete(data)).resolves.toBeUndefined()
   })
 })

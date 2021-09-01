174f2876c0c04e3823700dc6f4033124cfe9646d
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-01 10:31:44 -0300

    Created the delete route to /toDos

--- src/routes/toDos.routes.ts
+++ src/routes/toDos.routes.ts
@@ -3,16 +3,19 @@ import { Router } from 'express'
 // Controllers
 import CreateToDoController from '../controllers/toDo/CreateToDoController'
 import GetToDosController from '../controllers/toDo/GetToDosController'
+import DeleteToDoController from '../controllers/toDo/DeleteToDoController'
 
 // Middlewares
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const createToDo = new CreateToDoController()
 const getToDos = new GetToDosController()
+const deleteToDos = new DeleteToDoController()
 
 const toDosRoutes = Router()
 
 toDosRoutes.get('/', ensureAuthenticated, getToDos.handle)
 toDosRoutes.post('/', ensureAuthenticated, createToDo.handle)
+toDosRoutes.delete('/:id', ensureAuthenticated, deleteToDos.handle)
 
 export default toDosRoutes

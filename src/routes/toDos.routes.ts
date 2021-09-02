327bb937d4f4c493e379cbd606e3b7cb0e895dd5
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-02 09:43:31 -0300

    Created the PUT type request for the /toDos route

--- src/routes/toDos.routes.ts
+++ src/routes/toDos.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 // Controllers
 import CreateToDoController from '../controllers/toDo/CreateToDoController'
 import GetToDosController from '../controllers/toDo/GetToDosController'
+import UpdateToDoController from '../controllers/toDo/UpdateToDoController'
 import DeleteToDoController from '../controllers/toDo/DeleteToDoController'
 
 // Middlewares
@@ -10,12 +11,14 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const createToDo = new CreateToDoController()
 const getToDos = new GetToDosController()
+const updateToDos = new UpdateToDoController()
 const deleteToDos = new DeleteToDoController()
 
 const toDosRoutes = Router()
 
 toDosRoutes.get('/', ensureAuthenticated, getToDos.handle)
 toDosRoutes.post('/', ensureAuthenticated, createToDo.handle)
+toDosRoutes.put('/', ensureAuthenticated, updateToDos.handle)
 toDosRoutes.delete('/:id', ensureAuthenticated, deleteToDos.handle)
 
 export default toDosRoutes
